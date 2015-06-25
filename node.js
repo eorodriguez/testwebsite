a7456e90d513f333beff1178e3661ce3c122da17
eorodrig <eorodrig@ualberta.ca>
2015-06-24 20:36:08 -0600

    added node.js for devmod

---
+++ node.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('\'Edwin Rodriguez 3 node.js on Azure\' \n');
+}).listen(port);
